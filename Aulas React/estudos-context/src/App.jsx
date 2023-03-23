654e19ff6cef9fca73ef4d51b2147715812b3c3a
Jose Augusto <josejasfarias@gmail.com>
2023-03-23 17:00:15 -0300

    Criação do repositorio de estudos SoulCodeAcademy

--- Aulas React/estudos-context/src/App.jsx
+++ Aulas React/estudos-context/src/App.jsx
@@ -5,20 +5,34 @@ import { Propriedades } from './components/Propriedades/Propriedades';
 import { Home } from './pages/Home/Home';
 import { Login } from './pages/Login/Login';
 import { Perfil } from './pages/Perfil/Perfil';
+import { ThemeContext } from './contexts/ThemeContext';
+import { useState } from 'react';
 
 export function App() {
+  const [temaEscuro, settemaEscuro] = useState(false);
+
+  function alternar() {
+    if (temaEscuro === true) {
+      settemaEscuro(false);
+    } else {
+      settemaEscuro(true);
+    }
+  }
+
   return (
-  <BrowserRouter>
-  <NavBar />
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/perfil' element={<Perfil/>}/>
-    <Route path='/login' element={<Login/>}/>
-  </Routes>
-  </BrowserRouter>
+    <ThemeContext.Provider value={{temaEscuro: temaEscuro, alternar: alternar}}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   );
 }
-   /*    <Propriedades
+/*    <Propriedades
         cidade="Ubajara"
         estado="CE"
         preco={9}
